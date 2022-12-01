87305ef3083d72b5f33b2c6fc90efe8fa3b074a7
Dmitrii Mironov <dimamironov@mail.ru>
2022-12-01 14:51:55 +0300

    Tasks 1 completed

--- Task1/Task1.py
+++ Task1/Task1.py
@@ -2,5 +2,34 @@
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+import os
+import random
 
+os.system('cls')
 
+print('Найдем сумму чисел на нечетных позициях случайно сгенерированного списка.')
+
+n = int(input('Введите длину списка: '))
+
+randomlist = random.sample(range(0, 10), n)
+
+print()
+
+print(f'Получился такой список: {randomlist}.')
+
+print()
+
+def getSumOdds(inputlist):
+
+    sum = 0
+
+    for i in range(0, len(inputlist)):
+        if i%2 == 0:
+            pass
+
+        if i%2 != 0:
+            sum = sum + inputlist[i]
+        
+    return sum
+
+print(f'Сумма чисел на нечетных позициях списка выше равна: {getSumOdds(randomlist)}.')
