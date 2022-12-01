0d6ccefe883ee8f8efde38412d075a574ee13e53
Dmitrii Mironov <dimamironov@mail.ru>
2022-12-01 17:38:40 +0300

    Tasks 4 completed

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -4,4 +4,23 @@
 # 3 -> 11
 # 2 -> 10
 
+import os
 
+os.system('cls')
+
+print('Переведем десятичное число в двоичное.')
+
+n = int(input('Введите десятичное число: '))
+
+def ConvertToBin(number):
+
+    if number >= 1:
+        ConvertToBin(number // 2)
+    
+    print(number % 2, end = '')
+
+print()
+
+print('Получается такое двоичное число: ', end = '')
+    
+ConvertToBin(n)
